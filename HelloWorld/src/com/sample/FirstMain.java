56977f6c39c23a8e8f2166987b6d8d5f52746b97
my name is <myemail@myName>
2015-02-20 16:17:03 -0500

    third commit

--- HelloWorld/src/com/sample/FirstMain.java
+++ HelloWorld/src/com/sample/FirstMain.java
@@ -7,6 +7,8 @@ public class FirstMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("Date is " + new Date() );
+		
+		System.out.println("New Change");
 	}
 
 }
