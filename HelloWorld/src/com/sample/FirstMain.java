7353fa59cb40de4e4cc3179dabe509aad29540d4
my name is <myemail@myName>
2015-02-20 16:10:56 -0500

    second commit

--- HelloWorld/src/com/sample/FirstMain.java
+++ HelloWorld/src/com/sample/FirstMain.java
@@ -1,10 +1,12 @@
 package com.sample;
 
+import java.util.Date;
+
 public class FirstMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
-
+		System.out.println("Date is " + new Date() );
 	}
 
 }
