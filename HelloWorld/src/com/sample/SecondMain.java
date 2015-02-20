c917f67aaa8f5a1a996b917fe50004fa07ad6f1a
My Name is <MyName@My-Name>
2015-02-20 18:45:59 -0500

    first commit to 3rd branch

--- HelloWorld/src/com/sample/SecondMain.java
+++ HelloWorld/src/com/sample/SecondMain.java
@@ -5,6 +5,8 @@ public class SecondMain {
 	public static void main(String[] args) {
 		System.out.println("This is second main class");
 		System.out.println("heyy!");
+		
+		System.out.println("This is from third branch!");
 
 	}
 
